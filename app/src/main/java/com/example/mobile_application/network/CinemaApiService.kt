3a46ad083e52c97876ea9477882ea1142c94d409
Gilomyk <gilomyk@gmail.com>
2025-06-11 12:58:10 +0200

    added fetching hallType to repository

--- app/src/main/java/com/example/mobile_application/network/CinemaApiService.kt
+++ app/src/main/java/com/example/mobile_application/network/CinemaApiService.kt
@@ -2,10 +2,10 @@ package com.example.mobile_application.network
 
 import com.example.mobile_application.model.CinemaHallResponse
 import com.example.mobile_application.model.CinemaResponse
+import com.example.mobile_application.model.HallTypeResponse
 import com.example.mobile_application.model.SeatResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CinemaApiService {
@@ -14,6 +14,9 @@ interface CinemaApiService {
 
     @GET("/api/cinema_halls")
     suspend fun getCinemaHalls(@Query("page") page: Int?, @Query("cinema") cinemaId: Int?): Response<CinemaHallResponse>
+
+    @GET("/api/hall_types")
+    suspend fun getHallTypes(): Response<HallTypeResponse>
 
     @GET("/api/seats")
     suspend fun getSeats(@Query("page") page: Int?,@Query("hall") hallId: Int?): Response<SeatResponse>
--- app/src/main/java/com/example/mobile_application/repository/CinemaRepository.kt
+++ app/src/main/java/com/example/mobile_application/repository/CinemaRepository.kt
@@ -3,7 +3,7 @@ package com.example.mobile_application.repository
 import android.util.Log
 import com.example.mobile_application.model.Cinema
 import com.example.mobile_application.model.CinemaHall
-import com.example.mobile_application.model.Movie
+import com.example.mobile_application.model.HallType
 import com.example.mobile_application.network.ApiClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -66,5 +66,19 @@ class CinemaRepository {
         }
 
         return@withContext allHalls
+    }
+
+    suspend fun fetchHallTypes(): List<HallType>? = withContext(Dispatchers.IO) {
+        try {
+            val response = ApiClient.cinemaService.getHallTypes()
+            if (response.isSuccessful) {
+                return@withContext response.body()?.results
+            } else {
+                Log.e("MovieRepository", "API error: ${response.code()}")
+            }
+        } catch (e: Exception) {
+            Log.e("MovieRepository", "Exception: ${e.localizedMessage}")
+        }
+        return@withContext null
     }
 }
